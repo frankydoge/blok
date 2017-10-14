c933cb10af4195f2f2ab32d6085091ad833a3c0d
Justin Martin <justin.m146@gmail.com>
2017-10-14 14:15:55 +1100

    cant find import

--- src/modules/Panel/Panel.js
+++ src/modules/Panel/Panel.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 import '../../assets//Panel.css'
-import { Container } from '../../import'
+import { Container } from '../../../index'
 
 const Panel = (props) => {
   const {
