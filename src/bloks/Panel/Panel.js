674f43f28fb428e4e608f6c7fb57c0d47dafa05c
Justin Martin <justin.m146@gmail.com>
2017-10-16 16:46:04 +1100

    updated panel

--- src/bloks/Panel/Panel.js
+++ src/bloks/Panel/Panel.js
@@ -1,29 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
+import { Grid, Column } from '../../../index'
 import '../../blok.css'
 
 const Panel = (props) => {
   const {
-    position
+    heading,
+    linkText,
+    linkRef
   } = props
-  var panelClass = cx ({
-    'panel': true,
-    [`panel-${position}`]: position
-  })
   return (
-    <div className={panelClass}>
-      
-    </div>
+    <Grid compact color='red'>
+      <Column textAlign='middle'>
+        <h1>{props.heading}</h1>
+      </Column>
+      <Column />
+      <Column textAlign='right'>
+        <a href={props.linkRef}>{props.linkText}</a>
+      </Column>
+    </Grid>
   )
 }
 
-Panel.defaultProps = {
-  position: 'top'
+Panel.propTypes = {
+  heading: PropTypes.string,
+  linkText: PropTypes.string,
+  linkRef: PropTypes.string
 }
 
-Panel.propTypes = {
-  position: PropTypes.string
+Panel.defaultProps = {
+  heading: 'Title',
+  linkText: 'Link',
+  linkRef: '#'
 }
 
 export default Panel
