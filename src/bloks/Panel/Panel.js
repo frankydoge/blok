37e6507ce056eeb3ff7cd58f8e2128e1c920221b
Justin Martin <justin.m146@gmail.com>
2017-10-17 10:28:16 +1100

    added links to panel

--- src/bloks/Panel/Panel.js
+++ src/bloks/Panel/Panel.js
@@ -1,28 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
-import { Column, Grid, Heading } from '../../../index'
+import { Column, Grid, Heading, Link } from '../../../index'
 import '../../blok.css'
 
 const Panel = (props) => {
   const {
     color,
     heading,
+    link,
     side
   } = props
+  const linkData = props.link.map(function(link, id) {
+    return <Link ref={link.ref} text={link.text} key={id} active={link.active} />;
+  });
   if (side === true) {
     return (
       <Grid compact color={color}>
-        <Grid compact color={color}>
-          <Column textAlign='middle'>
-            <Heading font='body'>{props.heading}</Heading>
-          </Column>
-        </Grid>
-        <Grid compact color={color}>
-          <Column textAlign='right'>
-            {props.children}
-          </Column>
-        </Grid>
+        <Column>
+          <Grid compact color={color}>
+            <Column textAlign='middle'>
+              <Heading font='body'>{props.heading}</Heading>
+            </Column>
+          </Grid>
+          <Grid compact color={color}>
+            <Column textAlign='right'>
+              {props.linkData}
+            </Column>
+          </Grid>
+        </Column>
+        <Column>
+          {props.children}
+        </Column>
       </Grid>
     )
   } else {
@@ -33,7 +42,7 @@ const Panel = (props) => {
         </Column>
         <Column />
         <Column textAlign='right'>
-          {props.children}
+          {props.linkData}
         </Column>
       </Grid>
     )
@@ -43,6 +52,7 @@ const Panel = (props) => {
 Panel.propTypes = {
   color: PropTypes.string,
   heading: PropTypes.string,
+  link: PropTypes.object,
   side: PropTypes.bool
 }
 
