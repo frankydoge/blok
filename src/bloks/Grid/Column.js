da34ba09506f159415f07f5533946a3d41fcad7b
Justin Martin <justin.m146@gmail.com>
2017-10-16 16:09:46 +1100

    added color to column

--- src/bloks/Grid/Column.js
+++ src/bloks/Grid/Column.js
@@ -4,14 +4,26 @@ import cx from 'classnames'
 import '../../blok.css'
 
 const Column = (props) => {
+  const {
+    color
+  } = props
   var columnClass = cx ({
-    'column': true
+    'column': true,
+    [`color-background-${color}`]: color
   })
   return (
     <div className={columnClass} >
       {props.children}
     </div>
   )
+}
+
+Container.defaultProps = {
+  color: 'blue'
+}
+
+Container.propTypes = {
+  color: PropTypes.string
 }
 
 export default Column
