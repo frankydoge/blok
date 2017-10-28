2ac6e81f8f05cd55ded3891feb8cda3e313bd361
Justin Martin <justin.m146@gmail.com>
2017-10-28 18:27:22 +1100

    updated blok.header from old panel class names and removed additional side options

--- src/bloks/Blok/Header.js
+++ src/bloks/Blok/Header.js
@@ -9,16 +9,11 @@ const Panel = (props) => {
     className,
     color,
     heading,
-    link,
-    side
+    link
   } = props
-  var panelTopClass = cx ({
-    [`${className}`]: className,
-    [`panel-side-${side}`]: side
-  })
-  var panelSideClass = cx ({
-    [`${className}`]: className,
-    [`panel-side-${side}`]: side
+  var blokHeaderClass = cx ({
+    [`blok-header`]: true,
+    [`${className}`]: className
   })
   const linkData = link.map((data, key) =>
     <Link
@@ -28,68 +23,18 @@ const Panel = (props) => {
       active={data.active}
     />
   )
-  if (side === 'top') {
-    return (
-      <Grid color={color} raised>
-        <Grid.Row>
-          <Grid.Column width={2} textAlign='middle'>
-            <Heading font='body' size='regular' text={props.heading} />
-          </Grid.Column>
-          <Grid.Column width={6} textAlign='right' className={panelTopClass}>
-            {linkData}
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    )
-  } else if (side === 'right') {
-    return (
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width={7}>
-            {props.children}
-          </Grid.Column>
-          <Grid.Column width={1} textAlign='middle' color={color} className={panelSideClass}>
-            <Grid>
-              <Grid.Row>
-                <Grid.Column width={8}>
-                  <Heading font='body' size='regular' text={props.heading} />
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
-                <Grid.Column width={8} textAlign='left'>
-                  {linkData}
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    )
-  } else {
-    return (
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width={1} textAlign='middle' color={color} className={panelSideClass}>
-            <Grid>
-              <Grid.Row>
-                <Grid.Column width={7}>
-                  <Heading font='body' size='regular' text={props.heading} />
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
-                <Grid.Column width={8} textAlign='right'>
-                  {linkData}
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Grid.Column>
-          <Grid.Column width={6}>
-            {props.children}
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    )
-  }
+  return (
+    <Grid color={color} raised>
+      <Grid.Row>
+        <Grid.Column width={2} textAlign='middle'>
+          <Heading font='body' size='regular' text={props.heading} />
+        </Grid.Column>
+        <Grid.Column width={6} textAlign='right' className={blokHeaderClass}>
+          {linkData}
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  )
 }
 
 Panel.propTypes = {
